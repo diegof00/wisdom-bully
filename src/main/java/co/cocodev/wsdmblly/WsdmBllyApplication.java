ce3692880685045dca3b1e38527ae27b4df11857
Diego Reyes <dfreyes00@gmail.com>
2018-09-08 20:49:49 -0500

    project setup

---
+++ src/main/java/co/cocodev/wsdmblly/WsdmBllyApplication.java
@@ -0,0 +1,12 @@
+package co.cocodev.wsdmblly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdmBllyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsdmBllyApplication.class, args);
+    }
+}
---
+++ src/test/java/co/cocodev/wsdmblly/WsdmBllyApplicationTests.java
@@ -0,0 +1,16 @@
+package co.cocodev.wsdmblly;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WsdmBllyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
